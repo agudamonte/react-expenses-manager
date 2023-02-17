98bb6d2b7fdd2837d73418ac8650eb8b2dfc21fd
agudamonte <agu.damonte@gmail.com>
2023-02-17 16:11:23 -0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Car Insurance',
+    amount: 294.34,
+    date: new Date(2023, 1, 9),
+  },
+  {
+    id: 'e2',
+    title: 'Paper',
+    amount: 294.34,
+    date: new Date(2023, 1, 9),
+  },
+  {
+    id: 'e3',
+    title: 'books',
+    amount: 222.5,
+    date: new Date(2023, 1, 9),
+  },
+  {
+    id: 'e4',
+    title: 'helmet',
+    amount: 2400,
+    date: new Date(2023, 2, 9),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseDataHandler = (expense) => {
+    /* the clean way to update our state when it's 
+     based on an older snapshot of the same state */
+    setExpenses((prevState) => [expense, ...prevState]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseDataHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
